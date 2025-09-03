ee8952324541cfde2a668826f217f69b545d9524
nehasbabu <144600285+nehasbabu@users.noreply.github.com>
2025-09-03 14:47:29 +0530

    Update server.py

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ def hello():
 def guess(username):
     response1 = requests.get(url=f"https://api.agify.io?name={username}")
     response2 = requests.get(url=f"https://api.genderize.io?name={username}")
-    return render_template('index.html',name=username,gen=response2.json()['gender'],ag=response1.json()['age'],CURRENT_YEAR=datetime.now().year,n="neha")
+    return render_template('index.html',name=username.title(),gen=response2.json()['gender'],ag=response1.json()['age'],CURRENT_YEAR=datetime.now().year,n="neha")
 if __name__=="__main__":
+
     app.run(debug=True)
