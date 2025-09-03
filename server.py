d16ff75e252b47f61e50dbe4c33b9b1cafb9607c
nehasbabu <144600285+nehasbabu@users.noreply.github.com>
2025-09-03 14:44:06 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+from datetime import datetime
+app=Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello"
+@app.route("/guess/<username>")
+def guess(username):
+    response1 = requests.get(url=f"https://api.agify.io?name={username}")
+    response2 = requests.get(url=f"https://api.genderize.io?name={username}")
+    return render_template('index.html',name=username,gen=response2.json()['gender'],ag=response1.json()['age'],CURRENT_YEAR=datetime.now().year,n="neha")
+if __name__=="__main__":
+    app.run(debug=True)
